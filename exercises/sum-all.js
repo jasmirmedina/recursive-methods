6da7ada200869041ab4d48687bb02165e508698e
jasmirmedina <jasmirmedina56@gmail.com>
2025-02-17 02:42:03 -0600

    chore: min changes

--- exercises/sum-all.js
+++ exercises/sum-all.js
@@ -2,6 +2,8 @@
 //
 // Write a function called sumRange.
 // It will take a number and return the sum of all numbers from 1 up to the number passed in.
+//
+// Sample: sumRange(3) returns 6, since 1 + 2 + 3 = 6.
 
 function sumRange(n) {
   return n === 1 ? n : n + sumRange(n - 1);
