68b1b8ef041fdc9b72180347663a23ce84110fc6
jasmirmedina <jasmirmedina56@gmail.com>
2025-02-17 02:40:39 -0600

    chore: rewritten as one-liner

--- exercises/sum-all.js
+++ exercises/sum-all.js
@@ -4,11 +4,7 @@
 // It will take a number and return the sum of all numbers from 1 up to the number passed in.
 
 function sumRange(n) {
-  if(n === 1) {
-    return n;
-  }
-
-  return n + sumRange(n - 1);
+  return n === 1 ? n : n + sumRange(n - 1);
 }
 
 console.log(sumRange(6));
